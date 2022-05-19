c39b32552cbc21eba4d0f73c5603c20fb8a32a09
NandaPerovano <nandaperovanorm@gmail.com>
2022-05-18 22:59:48 -0300

    tela Configuração: adicionou datePicker

--- TabbarFilmesDeTerror/ConfiguracaoViewController.swift
+++ TabbarFilmesDeTerror/ConfiguracaoViewController.swift
@@ -11,16 +11,33 @@ class ConfiguracaoViewController: UIViewController {
 
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var pickerViewHumor: UIPickerView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var datePickerLabel: UILabel!
+    @IBOutlet weak var proximaDataLabel: UILabel!
     
-    var humores:[String] = ["Estou Feliz", "Estou triste", "Estou com medo"]
+    var humores:[String] = ["Estou Feliz", "Estou triste", "Estou com medo", "Estou ansioso", "Estou desapontado"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.dataSource = self
         pickerView.delegate = self
+        proximaDataLabel.text = "Pretende assistir o próximo filme dia:"
     }
     
+    @IBAction func assistirProximoFilme(_ sender: Any) {
+        
+        let dataEHora = datePicker.date
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC-3")
+        
+        let stringData = dateFormatter.string(from: dataEHora)
+        datePickerLabel.text = stringData
+    }
 }
+
 extension ConfiguracaoViewController: UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -29,10 +46,8 @@ extension ConfiguracaoViewController: UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return humores.count
     }
-    
-    
-    
 }
+
 extension ConfiguracaoViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return humores[row]
